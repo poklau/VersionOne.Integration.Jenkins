4b14d91bb4f8913cc1b76258adb1030c3b606e89
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-09-29 14:40:50 +0000

    added sdk libraries
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@30270 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ Sdk/install.bat
@@ -0,0 +1,4 @@
+SET V1_SDK_VERSION=8.0.200
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.APIClient.jar
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
+pause
