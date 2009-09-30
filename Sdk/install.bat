3835f6eeb0ee9dc02a39156f7a00715be8ec2672
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-09-30 08:00:46 +0000

    pom updated
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@30315 542498e7-56f8-f544-a0f2-a85913ac812a

--- Sdk/install.bat
+++ Sdk/install.bat
@@ -1,4 +1,4 @@
-SET V1_SDK_VERSION=8.0.200
+SET V1_SDK_VERSION=8.0-SNAPSHOT
 call mvn install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.APIClient.jar
 call mvn install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
 pause
