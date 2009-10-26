9d9d413fbf458854f39af5022b94b0628401a201
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-10-26 13:47:04 +0000

    Help for settings and extended Test connection added
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@31372 542498e7-56f8-f544-a0f2-a85913ac812a

--- Sdk/mvn-install-sdk.bat
+++ Sdk/mvn-install-sdk.bat
@@ -1,4 +1,6 @@
 SET V1_SDK_VERSION=8.0-SNAPSHOT
 call mvn install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.APIClient.jar
 call mvn install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
 pause
