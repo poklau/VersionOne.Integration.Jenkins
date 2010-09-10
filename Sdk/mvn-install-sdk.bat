97080bf9baa4d93f9bc8368cb08067d2734ecf96
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-09-10 17:01:10 +0000

    build fix cleanup: removed Maven external and script hacks
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@39567 542498e7-56f8-f544-a0f2-a85913ac812a

--- Sdk/mvn-install-sdk.bat
+++ Sdk/mvn-install-sdk.bat
@@ -1,7 +1,6 @@
 SET V1_SDK_VERSION=8.0-SNAPSHOT
-SET MAVEN_EXECUTABLE=..\apache-maven-2.2.1\bin\mvn.bat  
-call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.APIClient.jar
-call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
-call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
-call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.APIClient.jar
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
+call mvn install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
 pause
