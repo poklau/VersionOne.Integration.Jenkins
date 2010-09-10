a533286a0800a9f4dc676db216480bb1057826b1
Eugene <Eugene@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-09-10 16:49:38 +0000

    temp: build
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/Hudson@39566 542498e7-56f8-f544-a0f2-a85913ac812a

--- Sdk/mvn-install-sdk.bat
+++ Sdk/mvn-install-sdk.bat
@@ -1,5 +1,5 @@
 SET V1_SDK_VERSION=8.0-SNAPSHOT
-SET MAVEN_EXECUTABLE=.\apache-maven-2.2.1\bin\mvn.bat  
+SET MAVEN_EXECUTABLE=..\apache-maven-2.2.1\bin\mvn.bat  
 call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.APIClient.jar
 call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=objectmodel -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dfile=VersionOne.ObjectModel.jar
 call %MAVEN_EXECUTABLE% install:install-file -DgroupId=com.versionone -DartifactId=apiclient -Dversion=%V1_SDK_VERSION% -Dpackaging=jar -Dclassifier=sources -Dfile=VersionOne.JavaSDK.Source-8.0.zip
